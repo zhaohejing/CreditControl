c45d4379b1555a78f17438e06f18a6df254b7324
zhaohejing <454258314@qq.com>
2017-10-25 15:14:42 +0800

    初始化

--- src/YT.Web/Assets/src/router/index.js
+++ src/YT.Web/Assets/src/router/index.js
@@ -45,54 +45,43 @@ export const asyncRouterMap = [{
       },
       {
         path: '',
+        name: '采购管理',
+        icon: 'person-stalker',
+        component: {
+          render(c) {
+            return c('router-view');
+          }
+        },
+        children: [{
+            path: '/products',
+            name: '商品管理',
+            icon: 'person',
+            hidden: false,
+            component: r => require(['views/procurement/index'], r)
+          },
+          {
+            path: '/orders',
+            name: '订单管理',
+            icon: 'person-add',
+            component: r => require(['views/procurement/order'], r)
+          }
+        ]
+      },
+      {
+        path: '/customers',
         name: '客户管理',
-        icon: 'person-stalker',
-        component: {
-          render(c) {
-            return c('router-view');
-          }
-        },
-        children: [{
-            path: 'customer/client',
-            name: '用户',
-            icon: 'person',
-            hidden: false,
-            component: r => require(['views/customer/client'], r)
-          },
-          {
-            path: 'customer/charge',
-            name: '账户',
-            icon: 'person-add',
-            component: r => require(['views/customer/charge'], r)
-          }
-        ]
-      },
-      {
-        path: '',
-        name: '充值卡管理',
         icon: 'card',
-        component: {
-          render(c) {
-            return c('router-view');
-          }
-        },
-        children: [{
-          path: 'card/charge',
-          name: '卡片管理',
-          icon: '',
-          hidden: false,
-          component: r => require(['views/card/card'], r)
-        }, {
-          path: 'card/only',
-          name: '唯鲜卡管理',
-          icon: '',
-          hidden: false,
-          component: r => require(['views/card/onlymilk'], r)
-        }]
+        component: r => require(['views/card/card'], r)
       },
       {
+        path: '/series',
+        name: '系列管理',
+        icon: 'person-stalker',
+        component: r => require(['views/series/index'], r)
+      },
+  {
         path: '',
-        name: '推广管理',
+        name: '财务管理',
         icon: 'person-stalker',
         component: {
           render(c) {
@@ -100,20 +89,21 @@ export const asyncRouterMap = [{
           }
         },
         children: [{
-            path: 'generalize/promoters',
-            name: '推广员',
+            path: '/chargerecord',
+            name: '充值记录',
             icon: 'person',
             hidden: false,
-            component: r => require(['views/generalize/promoters'], r)
+            component: r => require(['views/finance/record'], r)
           },
           {
-            path: 'generalize/wechat',
-            name: '群发',
+            path: '/applyforcharge',
+            name: '充值申请记录',
             icon: 'person-add',
-            component: r => require(['views/generalize/wechat'], r)
+            component: r => require(['views/finance/apply'], r)
           }
         ]
       },
+
       {
         path: '',
         name: '权限管理',
@@ -124,43 +114,19 @@ export const asyncRouterMap = [{
           }
         },
         children: [{
-            path: 'system/role',
+            path: '/roles',
             name: '角色管理',
             icon: 'person',
             component: r => require(['views/manager/role'], r)
           },
           {
-            path: 'system/account',
+            path: '/users',
             name: '用户管理',
             icon: 'person-add',
             component: r => require(['views/manager/account'], r)
-          },
-          {
-            path: 'system/menu',
-            name: '菜单管理',
-            icon: 'person-add',
-            component: r => require(['views/manager/menu'], r)
           }
         ]
-      },
-      {
-        path: '',
-        name: '操作记录',
-        icon: 'person-stalker',
-        component: {
-          render(c) {
-            return c('router-view');
-          }
-        },
-        children: [{
-          path: 'log/audit',
-          name: '日志',
-          icon: 'person',
-          hidden: false,
-          component: r => require(['views/operation/audit'], r)
-        }]
       }
-
     ]
   },
   {
