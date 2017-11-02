595426f71b4b220b8d617292e4bc2851170bbc9b
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-11-02 15:33:05 +0800

    www

--- src/YT.Web/Assets/config/prod.env.js
+++ src/YT.Web/Assets/config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"http://192.168.0.202:22222"',
-    APP_ORIGIN: '"http://192.168.0.202:22222"'
+    BASE_API: '"http://172.17.45.250:9999"',
+    APP_ORIGIN: '"http://172.17.45.250:9999"'
 };
