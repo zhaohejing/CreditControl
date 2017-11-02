4fd6790312de38189aaed61e33ad4862307cd3a1
zhaohejing <454258314@qq.com>
2017-11-02 21:19:56 +0800

    aaa

--- src/YT.Web/Assets/src/views/procurement/modify-product.vue
+++ src/YT.Web/Assets/src/views/procurement/modify-product.vue
@@ -1,49 +1,49 @@
 <template>
     <div>
-        <Form ref="product" :model="current.productEditDto" :rules="ruleValidate" inline :label-width="120">
+        <Form ref='product' :model='current.productEditDto' :rules='ruleValidate' inline :label-width='120'>
             <Row>
-                <Col :span="18">
+                <Col :span='18'>
                 <Row>
-                    <Col :span="12">
-                    <FormItem label="商品名称" prop="productName">
-                        <Input v-model="current.productEditDto.productName" placeholder="商品名称"></Input>
+                    <Col :span='12'>
+                    <FormItem label='商品名称' prop='productName'>
+                        <Input v-model='current.productEditDto.productName' placeholder='商品名称'></Input>
                     </FormItem>
                     </Col>
-                    <Col :span="12">
-                    <FormItem label="价格" prop="price">
-                        <InputNumber :min="0" v-model="current.productEditDto.price" placeholder="价格"></InputNumber>
+                    <Col :span='12'>
+                    <FormItem label='价格' prop='price'>
+                        <InputNumber :min='0' v-model='current.productEditDto.price' placeholder='价格'></InputNumber>
                     </FormItem>
                     </Col>
                 </Row>
 
                 <Row>
-                    <Col :span="12">
-                    <FormItem label="一级分类">
-                        <Select @on-change="pick" v-model="current.productEditDto.levelOneId" style="width:150px">
-                            <Option v-for="item in Series.OneList" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                    <Col :span='12'>
+                    <FormItem label='一级分类'>
+                        <Select @on-change='pick' v-model='current.productEditDto.levelOneId' style='width:150px'>
+                            <Option v-for='item in Series.OneList' :value='item.id' :key='item.id'>{{ item.name }}</Option>
                         </Select>
                     </FormItem>
                     </Col>
-                    <Col :span="12">
-                    <FormItem label="二级分类">
-                        <Select v-model="current.productEditDto.levelTwoId" style="width:150px">
-                            <Option v-for="item in Series.TwoList" :value="item.id" :key="item.id">{{ item.name }}</Option>
+                    <Col :span='12'>
+                    <FormItem label='二级分类'>
+                        <Select v-model='current.productEditDto.levelTwoId' style='width:150px'>
+                            <Option v-for='item in Series.TwoList' :value='item.id' :key='item.id'>{{ item.name }}</Option>
                         </Select>
                     </FormItem>
                     </Col>
                 </Row>
 
                 <Row>
-                    <FormItem label="商品简介">
-                        <Input style="width:400px" v-model="current.productEditDto.description" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="商品简介"></Input>
+                    <FormItem label='商品简介'>
+                        <Input style='width:400px' v-model='current.productEditDto.description' type='textarea' :autosize='{minRows: 2,maxRows: 5}' placeholder='商品简介'></Input>
                     </FormItem>
                 </Row>
 
                 </Col>
-                <Col :span="6">
-                <Upload multiple type="drag" :on-error="error" :on-success="success" :headers="upload.headers" :action="upload.url">
-                    <div style="padding: 20px 0">
-                        <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+                <Col :span='6'>
+                <Upload multiple type='drag' :on-error='error' :on-success='success' :headers='upload.headers' :action='upload.url'>
+                    <div style='padding: 20px 0'>
+                        <Icon type='ios-cloud-upload' size='52' style='color: #3399ff'></Icon>
                         <p>点击或将文件拖拽到这里上传</p>
                     </div>
                 </Upload>
@@ -80,14 +80,14 @@ export default {
             current: {
                 productEditDto: {
                     id: this.productId,
-                    productName: "",
+                    productName: '',
                     price: 0,
                     levelOneId: 0,
                     levelTwoId: 0,
-                    description: "",
-                    content: "",
+                    description: '',
+                    content: '',
                     isActive: true,
-                    profile: ""
+                    profile: ''
                 }
             },
             upload: {
