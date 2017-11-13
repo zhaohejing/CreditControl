5f9b458bb758e1cba378674024a1fd58d657e08b
zhaohejing2017 <zhaohejing@zhj125.cn>
2017-11-13 16:39:33 +0800

    ww

--- src/YT.Web/Assets/src/views/procurement/pricing.vue
+++ src/YT.Web/Assets/src/views/procurement/pricing.vue
@@ -1,31 +1,40 @@
 <template>
-    <div>
-        <table style="width:100%;" border="2">
-            <thead>
-                    <tr> <td>序号</td>
-                    <td>公司名称</td>
-                    <td>联系人</td>
-                    <td>定价</td></tr>
-                   
-            </thead>
-            <tbody>
-                <tr v-for="(d,i) in list" :key="i">
-                    <td>{{++i}}</td>
-                    <td>{{d.customerName}}</td>
+<div class="statistical">
+        <div  class="settle">
+            <Row class="settleTop">
+                <Col span="24">客户定价</Col>
+            </Row>
+            <div class="settleMain">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th class="g-font14">序号</th>
+                            <th>公司名称</th>
+                            <th>联系人</th>
+                            <th>定价</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                          <tr v-for="(d,i) in list" :key="i">
+                    <td class="g-font14">{{++i}}</td>
+                    <td>{{d.productName}}</td>
                     <td>{{d.contact}}</td>
                     <td> <InputNumber  :precision="0" v-model="d.price"></InputNumber></td>
                 </tr>
-            </tbody>
-        </table>
-        <Row>
-            <Col :offset="8" :span="4">
-            <Button @click="back">返回</Button>
-            </Col>
-                <Col  :span="4">
-    <Button @click="commit" type="primary">提交</Button>
-            </Col>
-        </Row>
+                    </tbody>
+                </table>
+            </div>
+            <Row class='footbtn'>
+                  <Col offset="6" span='4'>
+                <Button  type='primary' @click="back">返回</Button>
+                </Col>
+                <Col offset="2" span='4'>
+                <Button type='primary' @click="commit">完成</Button>
+                </Col>
+            </Row>
+        </div>
     </div>
+   
 </template>
 <script>
 import { prices, pricing } from "api/products";
@@ -71,3 +80,79 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+.statistical {
+  height: 100%;
+}
+.settle {
+  .settleTop {
+    width: 100%;
+    font-size: 18px;
+    color: #4c5b70;
+    margin-bottom: 1px;
+  }
+  .settleMain {
+    width: 100%;
+    text-align: center;
+    .table {
+      width: 100%;
+      margin: 0 auto;
+      border-collapse: collapse;
+      margin-bottom: 4px;
+      thead {
+        background: #f5f5f5;
+        tr {
+          height: 48px;
+          line-height: 48px;
+          border-bottom: none;
+        }
+        th {
+          color: #8d919a;
+          font-size: 14px;
+          font-weight: normal;
+          width: 100px;
+          padding-left: 30px;
+          text-align: left;
+          select {
+            width: 163px;
+            font-size: 14px;
+          }
+          .ivu-select-selection {
+            border-radius: 0;
+          }
+        }
+      }
+      tr {
+        height: 70px;
+        border-bottom: 1px solid #ebebeb;
+        td {
+          font-size: 18px;
+          color: #474e5c;
+          width: 187px;
+          padding-left: 30px;
+          text-align: left;
+        }
+        .g-font14 {
+          font-size: 14px;
+          width: 100px;
+        }
+      }
+    }
+  }
+  .footbtn {
+    margin-top: 120px;
+    text-align: center;
+    button {
+      width: 200px;
+      height: 40px;
+      text-align: center;
+      line-height: 40px;
+      background: #679fec;
+      border-radius: 0;
+      border: 1px solid #679fec;
+      font-size: 14px;
+      padding: 0;
+    }
+  }
+}
+</style>
